22288d85d3663d53e12d4cc2c8b8e11e7871c3c0
yaroslavtsevam <yaroslavtsevam@gmail.com>
2020-02-14 14:06:35 +0300

    ttr

--- code/TT_ttr_vs_tt.R
+++ code/TT_ttr_vs_tt.R
@@ -1,19 +1,28 @@
 
 data  = AllData %>% filter(Site == "BOLOTNAYA")
 
-TTR = AllData %>% filter(Species == "TTR")
+TTR = data %>% filter(Species == "TTR")
  data$Species %>% unique()
 
-tair_pic = ggplot(data=data)+
-  geom_point(aes(x=as.Date(time),y=ta),color=2, size=.1, alpha=1/20)+
-  geom_point(aes(x=as.Date(time),y=tair),color=3, size=.1, alpha=1/20)+
-  geom_smooth(aes(x=as.Date(time),y=ta), model="loess", color=2,span=.01)+
-  geom_smooth(aes(x=as.Date(time),y=tair), model="loess", color=3,span=.1)+
+sumdata  = data %>% group_by(Species, time) %>% summarise(tair = mean(tair,na.rm = T),rh = mean(rh,na.rm = T))
+ 
+ tair_pic = ggplot(data=data)+
+  geom_point(aes(x=as.Date(time),y=tair, color = Species), size=.1, alpha=1/20)+
+  geom_smooth(aes(x=as.Date(time),y=tair, color = Species), model="loess",span=.1)+
   scale_x_date(date_breaks = "1 month", date_labels = "%b", date_minor_breaks = "1 week",
                limits = c(as.Date("01.05.2019", "%d.%m.%Y"),NA), name = NULL)+
   scale_y_continuous( limits = c(0,33), name = expression(Температура~" "~воздуха~" "~С^o))+
   theme_bw()
-ggsave("Comparison_of_tair_in_RUDNVvsMSU.png", tair_pic,device = "png",width=5.83,height=4.13,units="in")
+ggsave("Comparison_of_TTR_vs_TT_Bolotnaya.png", tair_pic,device = "png",width=5.83,height=4.13,units="in")
+
+rh_pic = ggplot(data=data)+
+  geom_point(aes(x=as.Date(time),y=rh, color = Species), size=.1, alpha=1/20)+
+  geom_smooth(aes(x=as.Date(time),y=rh, color = Species), model="loess",span=.1)+
+  scale_x_date(date_breaks = "1 month", date_labels = "%b", date_minor_breaks = "1 week",
+               limits = c(as.Date("01.05.2019", "%d.%m.%Y"),NA), name = NULL)+
+  scale_y_continuous( limits = c(30,100), name = expression(Relative~" "~humidity~" "~С^o))+
+  theme_bw()
+ggsave("Comparison_of_TTR_vs_TT_Bolotnaya.png", tair_pic,device = "png",width=5.83,height=4.13,units="in")
 
 rh_pic = ggplot(data = data)+
   geom_point(aes(x=as.Date(time),y=Rh),color=2, size=.1, alpha=1/20)+
