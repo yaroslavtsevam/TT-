4af4762e02b7a96d8a6a98db860056f49db68333
yaroslavtsevam <yaroslavtsevam@gmail.com>
2020-02-14 13:46:00 +0300

    descr update

--- code/TT_get_all_Moscow_data.R
+++ code/TT_get_all_Moscow_data.R
@@ -59,7 +59,7 @@ SHERdata=TTcalc_site("http://naturetalkers.altervista.org/C1870015/ttcloud.txt",
                      "data/full_TT_desc.csv",
                      "SCHERBINKA")
 
-GRDNTdata  = TTcalc_site("http://naturetalkers.altervista.org/C18A0026/ttcloud.txt",
+GRDNdata  = TTcalc_site("http://naturetalkers.altervista.org/C18A0026/ttcloud.txt",
                         installation_start,
                         import_folder_name = NULL,
                         first_imported_dates_reconstructed = F,
@@ -116,29 +116,6 @@ AllData = rbind(
 
 data = RUDNdata[[2]]
 data = left_join(data,meteo_data, by=c("year","doy","hour"))
-
-tair_pic = ggplot(data=data)+
-  geom_point(aes(x=as.Date(time),y=ta),color=2, size=.1, alpha=1/20)+
-  geom_point(aes(x=as.Date(time),y=tair),color=3, size=.1, alpha=1/20)+
-  geom_smooth(aes(x=as.Date(time),y=ta), model="loess", color=2,span=.01)+
-  geom_smooth(aes(x=as.Date(time),y=tair), model="loess", color=3,span=.1)+
-  scale_x_date(date_breaks = "1 month", date_labels = "%b", date_minor_breaks = "1 week",
-               limits = c(as.Date("01.05.2019", "%d.%m.%Y"),NA), name = NULL)+
-  scale_y_continuous( limits = c(0,33), name = expression(Температура~" "~воздуха~" "~С^o))+
-  theme_bw()
-ggsave("Comparison_of_tair_in_RUDNVvsMSU.png", tair_pic,device = "png",width=5.83,height=4.13,units="in")
-
-rh_pic = ggplot(data = data)+
-  geom_point(aes(x=as.Date(time),y=Rh),color=2, size=.1, alpha=1/20)+
-  geom_point(aes(x=as.Date(time),y=rh),color=3, size=.1, alpha=1/20)+
-  geom_smooth(aes(x=as.Date(time),y=Rh), model="loess", color=2,span=.01)+
-  geom_smooth(aes(x=as.Date(time),y=rh), model="loess", color=3,span=.1)+
-  scale_x_date(date_breaks = "1 month", date_labels = "%b", date_minor_breaks = "1 week",
-               limits = c(as.Date("01.05.2019", "%d.%m.%Y"),NA), name = NULL)+
-  scale_y_continuous( limits = c(0,100), name = expression(Влажность~" "~воздуха~" "~"%"))+
-  theme_bw()
-
-ggsave("Comparison_of_rh_in_RUDNVvsMSU.png", rh_pic,device = "png",width=5.83,height=4.13,units="in")
 
 
 
